d1801eb2e11116da231164dc5e9b781bb64c4471
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-04-11 20:15:43 +0200

    Add axios and create post backend request function

---
+++ frontend/src/network/api/forum-posts.ts
@@ -0,0 +1,13 @@
+import { ForumPost } from '@/models/forum-post';
+import api from '@/network/axiosInstance';
+
+interface CreateForumPost {
+	slug: string;
+	title: string;
+	body: string;
+}
+
+export async function createForumPost(input: CreateForumPost) {
+	const response = await api.post<ForumPost>('/posts', input);
+	return response.data;
+}
